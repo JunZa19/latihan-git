c31720752da576617f668499af6c0e34d5681e54
JunZa19 <juniorzaitun@gmail.com>
2022-03-22 15:15:50 +0700

    menambahkan Biodata.php

---
+++ Biodata.php
@@ -0,0 +1,14 @@
+<?php
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Junior";
+        echo"<p> Berikut adakah Biodata saya</p>";
+        echo"<p>Nama : Junior Panderaya</p>";
+        echo"NIM : 17190159";
+        echo"<p>Kelas : 17.6A.25</p>";
+        echo"Fakultas : Teknologi Informasi";
+    }
+}
